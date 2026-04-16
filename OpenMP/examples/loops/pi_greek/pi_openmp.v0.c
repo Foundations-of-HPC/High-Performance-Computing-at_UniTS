3cdb96bd95d35d458637ae7a5ad4a2eaf19f3448
luca <luca.tornatore@inaf.it>
2026-04-16 10:45:38 +0200

    .

--- OpenMP/examples/loops/pi_greek/pi_openmp.v0.c
+++ OpenMP/examples/loops/pi_greek/pi_openmp.v0.c
@@ -38,6 +38,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <omp.h>
 
 #define DEFAULT 1000000
--- OpenMP/examples/loops/pi_greek/pi_openmp.v1.c
+++ OpenMP/examples/loops/pi_greek/pi_openmp.v1.c
@@ -39,6 +39,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <omp.h>
 
 #define DEFAULT 1000000
